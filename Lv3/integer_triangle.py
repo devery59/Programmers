7bcc58a5ad5a0261542e130adc21b4412e72179e
Kristopher <86634292+devery59@users.noreply.github.com>
2023-04-21 21:37:35 +0900

    Update integer_triangle.py

--- Lv3/integer_triangle.py
+++ Lv3/integer_triangle.py
@@ -1,4 +1,5 @@
 # idea : 맨 위부터 시작하여 아래로 내려가면서 바로 수를 누적해서 더해 현재 위치에 다다를 수 있는 경로중 최대값과 현재 위치 값을 더한다.
+# new knowledge : enumerate -> list에서 index와 값을 뽑아주는 함수. dict 에서 key value 뽑는것처럼 사용
 def solution(triangle):
     graph = [[] for _ in range(len(triangle))] # 누적된 결과를 저장하기 위한 Adjacency Matrix
     for index, values in enumerate(triangle): # triangle array에서 값을 하나씩 추출하여 이전 경로까지의 최대값과 더하기
